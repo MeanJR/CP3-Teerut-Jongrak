8d5873ab1cb0e9fa29555f66eb965cc8005303f8
MeanJR <meaneverdie@gmail.com>
2020-09-11 23:55:29 +0700

    Summit Exercise4

---
+++ assignments/Exercise4_Teerut_J.py
@@ -0,0 +1,14 @@
+
+scoreFE = 50   # Foundation English Score
+scoreGB = 70   # General Business Score
+scoreICS = 75  # Introduction to Computer Systems Score
+scoreCP = 90   # Computer Programming Score
+
+print(" ============= Your Score ============")
+
+print("Foundation English :              ", scoreFE)
+print("General Business :                ", scoreGB)
+print("Introduction to Computer Systems :", scoreICS)
+print("Computer Programming :            ", scoreCP)
+
+print(" =====================================")
