323dcacd4172414b4758b5f284cbdce5b4895a3c
MeanJR <meaneverdie@gmail.com>
2020-09-19 01:29:35 +0700

    summit Exercise5

---
+++ assignments/Exercise5_1_Teerut_J.py
@@ -0,0 +1,11 @@
+# input 2  Number
+
+number1 = int(input("Input Number 1:"))
+number2 = int(input("Input Number 2:"))
+
+# calculate Number & represent result
+
+print(number1,"+",number2,"=",number1+number2)
+print(number1,"-",number2,"=",number1-number2)
+print(number1,"*",number2,"=",number1*number2)
+print(number1,"/",number2,"=",int(number1/number2))
---
+++ assignments/Exercise5_2_Teerut_J.py
@@ -0,0 +1,11 @@
+# input value
+
+distance = float(input("Input distance in kilometer: "))
+time = float(input("Input time in hour: "))
+
+# Calculate speed kilometer per hour
+
+v = int(distance/time)  # Speed in km/hr
+# output
+
+print(v, "km/h")
